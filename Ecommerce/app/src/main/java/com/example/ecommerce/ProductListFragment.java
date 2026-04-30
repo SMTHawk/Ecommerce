64ba861eafe8cfae39e8675d3a2e2355fdfe5a3d
SMTHawk <157442319+SMTHawk@users.noreply.github.com>
2026-04-29 23:35:42 -0500

    Update ProductListFragment.java

--- Ecommerce/app/src/main/java/com/example/ecommerce/ProductListFragment.java
+++ Ecommerce/app/src/main/java/com/example/ecommerce/ProductListFragment.java
@@ -1,1 +1,31 @@
 package com.example.ecommerce;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import java.util.List;
+
+public class ProductListFragment extends Fragment {
+  private ProductRepository repository;
+
+  @Nullable
+  @Override
+  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    repository = new ProductRepository();
+
+    View view = inflater.inflate(android.R.layout.list_content, container, false);
+
+    displayProducts(repository.getProducts());
+    return view;
+  }
+
+  public void displayProducts(List<Product> products) {
+  }
+
+  public void onProductSelected(int productId) {
+  }
+}
