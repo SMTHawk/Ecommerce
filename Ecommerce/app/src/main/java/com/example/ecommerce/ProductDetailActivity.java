17e01474621d1ccaa5bd0c401ac31856970b649a
SMTHawk <157442319+SMTHawk@users.noreply.github.com>
2026-04-29 23:48:17 -0500

    Update ProductDetailActivity.java

--- Ecommerce/app/src/main/java/com/example/ecommerce/ProductDetailActivity.java
+++ Ecommerce/app/src/main/java/com/example/ecommerce/ProductDetailActivity.java
@@ -1,1 +1,27 @@
 package com.example.ecommerce;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProductDetailActivity extends AppCompatActivity {
+  private ProductRepository repository;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    repository = new ProductRepository();
+
+    int productId = getIntent().getIntExtra("PRODUCT_ID", -1);
+    if (productId !+ -1) {
+      loadProductDetails(productId);
+    }
+  }
+
+  private void loadProductDetails(int productId) {
+    Product product = repository.getProductById(productID);
+    if (product != null) {
+      setTitle(product.getName());
+    }
+  }
+}
