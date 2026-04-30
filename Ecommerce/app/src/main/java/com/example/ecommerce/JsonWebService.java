1849d91aed19248d868073585686bc992c146266
SMTHawk <157442319+SMTHawk@users.noreply.github.com>
2026-04-30 00:11:43 -0500

    Update and rename JsonWebServer.java to JsonWebService.java

---
+++ Ecommerce/app/src/main/java/com/example/ecommerce/JsonWebService.java
@@ -0,0 +1,12 @@
+package com.example.ecommerce;
+
+public class JsonWebService {
+
+  public String fetchProducts() {
+    return "[]";
+  }
+
+  public String fetchProductDetails(int id) {
+    return "{}";
+  }
+}
