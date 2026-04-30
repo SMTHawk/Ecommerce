60d7dd236abe1f6c1a5bef8e40285a8fc58bbfc5
SMTHawk <157442319+SMTHawk@users.noreply.github.com>
2026-04-30 00:09:59 -0500

    Update ProductDetailActivity.java

--- Ecommerce/app/src/main/java/com/example/ecommerce/ProductDetailActivity.java
+++ Ecommerce/app/src/main/java/com/example/ecommerce/ProductDetailActivity.java
@@ -1,6 +1,7 @@
 package com.example.ecommerce;
 
 import android.os.Bundle;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProductDetailActivity extends AppCompatActivity {
@@ -9,19 +10,26 @@ public class ProductDetailActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    TextView detailsView = new TextView(this);
+    setContentView(detailsView);
 
     repository = new ProductRepository();
 
     int productId = getIntent().getIntExtra("PRODUCT_ID", -1);
     if (productId !+ -1) {
-      loadProductDetails(productId);
+      loadProductDetails(productId, detailsView);
     }
   }
 
   private void loadProductDetails(int productId) {
     Product product = repository.getProductById(productID);
     if (product != null) {
-      setTitle(product.getName());
+      String info = "Name: " + product.getName() + "\n" + 
+        "Category: " + product.getCategory() + "\n" +
+        "Price: $" + product.getPrice() + "\n" +
+        "Ships From: " + product.getShipFrom();[cite: 2]
+      view.setText(info);
+      setTitle(product.getName());[cite: 2]
     }
   }
 }
