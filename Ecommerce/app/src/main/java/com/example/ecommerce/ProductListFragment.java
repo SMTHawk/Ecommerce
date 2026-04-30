a071d2f6781cb9f1511d0c0f7df8ab6e04f1d1cf
SMTHawk <157442319+SMTHawk@users.noreply.github.com>
2026-04-30 01:11:51 -0500

    Add import for LayoutInflater in ProductListFragment

--- Ecommerce/app/src/main/java/com/example/ecommerce/ProductListFragment.java
+++ Ecommerce/app/src/main/java/com/example/ecommerce/ProductListFragment.java
@@ -2,6 +2,7 @@ package com.example.ecommerce;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
