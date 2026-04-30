35bd5b4e6084594b4d66c174a37f4c28cafc2d07
SMTHawk <157442319+SMTHawk@users.noreply.github.com>
2026-04-29 23:26:43 -0500

    Update DataVirtualization.java

--- Ecommerce/app/src/main/java/com/example/ecommerce/DataVirtualization.java
+++ Ecommerce/app/src/main/java/com/example/ecommerce/DataVirtualization.java
@@ -1,1 +1,17 @@
 package com.example.ecommerce;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataVirtualization {
+  private List<Product> memoryCache = new ArrayList<>();
+
+  public void cacheProducts(List<Product> products) {
+    this.memoryCache.clear();
+    this.memoryCache.addAll(products);
+  }
+
+  public List<Product> getCachedProducts() {
+    return memoryCache;
+  }
+}
